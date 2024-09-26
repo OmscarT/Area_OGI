6d37b84e3ce859f1cef67668aeb5d977057b7825
OmscarT <oscartorresramirez498@gmail.com>
2024-09-26 11:06:34 -0600

    nuevo codigo oscar

--- src/GUI_FIguras.java
+++ src/GUI_FIguras.java
@@ -76,6 +76,7 @@ public class GUI_FIguras extends JFrame {
 		JButton btnArea1_2 = new JButton("Área");
 		btnArea1_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				// este es un comentario de mi programa para el git 
 				try {
 					// Captura de datos del usuario
 					float base = Float.parseFloat(JOptionPane.showInputDialog(null, "Dame la base del rectángulo:"));
